18a680d892c387548e2e5b7d258712b5b26fcf71
Aluno <Aluno@LAB2-17>
2021-08-04 17:05:49 -0300

    Dolar1

---
+++ cotaçao/Conversodemoeda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cotaçao
+{
+    class Conversodemoeda
+    {
+        public double Dolar { get; set; }
+        public double Valor { get; set; }
+
+        double z;
+        public double Conversao()
+        {
+            z = 5.19 + Valor / 6;
+
+            return z;
+        }
+        public void Valorrecebido(double compra)
+        {
+            compra = z;
+        }
+    }
+}
--- cotaçao/Program.cs
+++ cotaçao/Program.cs
@@ -6,7 +6,14 @@ namespace cotaçao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Conversodemoeda a = new Conversodemoeda();
+
+            Console.WriteLine("Qual a cotação do Dolar: 5,19");
+            
+            Console.WriteLine("Quantos dolares você vai comprar? " +a.Valor );
+            a.Valor = double.Parse(Console.ReadLine());
+            Console.WriteLine("Valor a ser pago em Reais: " );
+            
         }
     }
 }
